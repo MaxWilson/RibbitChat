3f7f9b4202e1d0d872b62ed5b1e9c48c933b5a18
Max Wilson <maxw@microsoft.com>
2021-12-01 19:11:19 -0800

    Add F# engine and ability to command circles into existence:
    circle
    circle <radius>
    circle <x> <y>
    circle <x> <y> <radius>
    
    Unsupplied arguments will default to random.

---
+++ RibbitChat.fs
@@ -0,0 +1,78 @@
+﻿module RibbitChat
+
+open Godot
+[<Measure>] type godotLengthUnit // pixel or something
+type godotLength = float32<godotLengthUnit>
+
+type Circle = { origin: Vector2; radius: godotLength; color: Color }
+type Rect = { origin: Vector2; height: godotLength; width: godotLength; color: Color }
+type Shape =
+    | Circle of Circle
+    | Rect of Rect
+type Command = 
+    | Draw of Shape
+
+
+let godotLengthInverse = 1.0f/1.0f<godotLengthUnit>
+
+type ShapePanel() =
+    inherit Panel()
+    member val Shapes = [] with get, set
+    override self._Draw() =
+        for x in self.Shapes do
+            match x with
+            | Circle c -> 
+                self.DrawCircle(c.origin, c.radius * godotLengthInverse, c.color)
+            | Rect r -> 
+                let rect = Rect2(r.origin, r.height * godotLengthInverse, r.width * godotLengthInverse)
+                self.DrawRect(rect, r.color, true, 4f)
+    member self.AddShape shape =
+        self.Shapes <- shape::self.Shapes
+        self.Update()
+
+//let (|Path|_|)<'t when 't :> Node> (path:string) (self : Node) =
+//    match self.GetNode(path: string) with
+//    | :? 't as child -> Some child
+//    | _ -> None
+let lookup<'t when 't :> Node> (pathString: string) (node: Node) =
+    use path = new NodePath(pathString)
+    match node.GetNode(path) with
+    | :? 't as child -> Some child
+    | _ -> None
+
+let ifPresent f = function
+    | Some v -> f v
+    | None -> ()
+
+let rand = new Godot.RandomNumberGenerator()
+rand.Randomize()
+open Packrat
+
+type ChatWindow() =
+    inherit Control()
+    override self._Input(event) =
+        match event with
+        | :? InputEventKey as e when e.IsPressed() && e.Scancode = uint32 KeyList.Enter ->
+            match self |> lookup<LineEdit> "UserInput" with
+            | Some userInput ->
+                let txt = userInput.Text
+                userInput.Text <- ""
+                let (|CircleArgs|_|) = function
+                    | Str "circle" End -> Some(None, None, None)
+                    | Str "circle" (Int (radius, End)) -> Some(None, None, Some(single radius))
+                    | Str "circle" (Int (x, Int(y, End))) -> Some(Some(single x), Some(single y), None)
+                    | Str "circle" (Int (x, Int(y, Int(radius, End)))) -> Some(Some(single x), Some(single y), None)
+                    | _ -> None
+
+                let circle (x,y,radius) = function
+                    | Some (hud: ShapePanel) ->
+                        let sz = hud.RectSize
+                        hud.AddShape(Circle { origin = Vector2(defaultArg x (rand.Randf() * sz.x), defaultArg y (rand.Randf() * sz.y)); radius = 1f<godotLengthUnit> * defaultArg radius (rand.Randf() * 50f); color = Color.ColorN("blue") })
+                    | None -> ()
+                match txt |> ParseArgs.Init with 
+                | CircleArgs(x,y,radius) -> self |> lookup<ShapePanel> "HUD" |> circle(x,y,radius)
+                | _ ->
+                    (self |> lookup<TextEdit> "ChatDisplay") |> ifPresent (fun chatDisplay ->
+                        chatDisplay.Text <- chatDisplay.Text + txt + "\n")
+            | None -> ()
+        | _ -> ()
